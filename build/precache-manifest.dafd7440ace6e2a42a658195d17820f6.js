9c1eb176c5a816fd0045804bb2cfee5afa195d12
Ouzii <oskari.laaja@gmail.com>
2019-02-04 15:20:21 +0200

    Added build to git

---
+++ build/precache-manifest.dafd7440ace6e2a42a658195d17820f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31774344a061253fb584",
+    "url": "/static/js/main.31774344.chunk.js"
+  },
+  {
+    "revision": "5dcbecee6739915cbd6f",
+    "url": "/static/js/1.5dcbecee.chunk.js"
+  },
+  {
+    "revision": "31774344a061253fb584",
+    "url": "/static/css/main.80c4786e.chunk.css"
+  },
+  {
+    "revision": "62b8e5b5f009d2113eb447f2ae7ea919",
+    "url": "/index.html"
+  }
+];
